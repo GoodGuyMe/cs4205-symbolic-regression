8f712bcec545ca3a1604f29bbf2bb66b6dfccfcb
Matthijs de Goede <m.b.j.degoede@student.tudelft.nl>
2024-06-06 13:48:50 +0200

    Refactor finite differencing methods and add differencing for constants

--- src/variation.py
+++ src/variation.py
@@ -3,18 +3,19 @@ import numba as nb
 from numba import types as nty
 import math
 
+
 def get_variation_fn(
-    population_size: int,
-    max_expression_size: int,
-    num_constants: int,
-    library_size: int,
-    p_crossover: float,
-    scaling_factor: float,
-    linear_scaling: bool,
-    evaluate_individual: callable,
-    evaluate_population: callable,
-    learning_rate: float = 0.01,
-    epsilon: float = 0.00001
+        population_size: int,
+        max_expression_size: int,
+        num_constants: int,
+        library_size: int,
+        p_crossover: float,
+        scaling_factor: float,
+        linear_scaling: bool,
+        evaluate_individual: callable,
+        evaluate_population: callable,
+        learning_rate: float = 0.01,
+        epsilon: float = 0.00001
 ):
     # @nb.jit((
     #     nty.Array(nty.float32, 2, "C"),
@@ -32,9 +33,11 @@ def get_variation_fn(
 
         # Implement gradient-based local search to optimize the structures of a genetic programming population using gradient with finite differences
 
-        temp_trial_structures = np.zeros_like(trial_structures)
-        temp_trial_fitness = np.zeros_like(trial_structures)
+        temp_trial_structs = np.zeros_like(trial_structures)
+        temp_trial_fit = np.zeros_like(trial_fitness)
+        temp_trial_consts = np.zeros_like(trial_constants)
 
+        ########################################## MUTATION ##############################################
 
         for i in range(population_size):
             r0 = r1 = r2 = i
@@ -42,19 +45,20 @@ def get_variation_fn(
             while r1 == i or r1 == r0:              r1 = rng.integers(0, population_size)
             while r2 == i or r2 == r0 or r2 == r1:  r2 = rng.integers(0, population_size)
             # randomly select one index for which mutation takes place for sure
-            j_rand: np.int32  = rng.integers(0, max_expression_size + num_constants)
+            j_rand: np.int32 = rng.integers(0, max_expression_size + num_constants)
 
             # construct trial population
             for j in range(structures.shape[1]):
                 # perform crossover on selected index j_rand with proba 1 or with proba p_crossover on other indices
                 if rng.random() < p_crossover or j == j_rand:
-                    temp_trial_structures[i, j] = structures[r0, j] + scaling_factor * (structures[r1, j] - structures[r2, j])
+                    temp_trial_structs[i, j] = structures[r0, j] + scaling_factor * (
+                            structures[r1, j] - structures[r2, j])
                     # repair as per Eq 8 (https://doi.org/10.1145/1389095.1389331) to arive at an integer that corresponds to an operator or variable
-                    v_abs = np.abs(temp_trial_structures[i, j])
+                    v_abs = np.abs(temp_trial_structs[i, j])
                     v_floored_abs = np.floor(v_abs)
-                    temp_trial_structures[i, j] = (v_floored_abs % library_size) + (v_abs - v_floored_abs)
+                    temp_trial_structs[i, j] = (v_floored_abs % library_size) + (v_abs - v_floored_abs)
                 else:
-                    temp_trial_structures[i, j] = structures[i, j]
+                    temp_trial_structs[i, j] = structures[i, j]
 
             if j_rand > max_expression_size:
                 j_rand -= max_expression_size
@@ -63,96 +67,85 @@ def get_variation_fn(
                 # perform crossover on selected index j_rand with proba 1 or with proba p_crossover on other indices
                 if rng.random() < p_crossover or j == j_rand:
                     # as these are constants, no repair is required
-                    trial_constants[i, j] = constants[r0, j] + scaling_factor * (constants[r1, j] - constants[r2, j])
+                    temp_trial_consts[i, j] = constants[r0, j] + scaling_factor * (constants[r1, j] - constants[r2, j])
                 else:
-                    trial_constants[i, j] = constants[i, j]
+                    temp_trial_consts[i, j] = constants[i, j]
 
-        # evaluate_population(temp_trial_structures, trial_constants, temp_trial_fitness, X, y, linear_scaling)
-        finite_difference_method = 'central'  # 'forward' or 'central' or 'backward'
+        ########################################## LOCAL SEARCH ##############################################
 
-        gradients = np.zeros_like(trial_structures)
-        for i in range(trial_structures.shape[0]):
-            for j in range(trial_structures.shape[1]):
-                # Perturb the structure positively and negatively
-                if finite_difference_method == 'forward':
-                    # f'(x) = 1/h * (f(x + h) - f(x)) : forward difference method
-                    original_value = temp_trial_structures[i, j]
-                    temp_trial_structures[i, j] = original_value + epsilon
-                    evaluate_individual(temp_trial_structures[i], trial_constants[i], temp_trial_fitness[i], X, y, linear_scaling)
-                    fitness_plus = temp_trial_fitness[i][0]
-                    temp_trial_structures[i, j] = original_value
-                    evaluate_individual(temp_trial_structures[i], trial_constants[i], temp_trial_fitness[i], X, y, linear_scaling)
-                    fitness_minus = temp_trial_fitness[i][0]
-                    # Calculate the gradient using finite differences
-                    if not math.isinf(fitness_plus) and not math.isinf(fitness_minus):
-                        gradients[i, j] = ((fitness_plus - fitness_minus) / (epsilon))
-                    # else:
-                    #     print("inf")
-                    # Reset the structure to its original value
-                    temp_trial_structures[i, j] = original_value
-                elif finite_difference_method == 'backward':
-                    # f'(x) = 1/h * (f(x) - f(x - h)) : backward difference method
-                    original_value = temp_trial_structures[i, j]
-                    temp_trial_structures[i, j] = original_value
-                    evaluate_individual(temp_trial_structures[i], trial_constants[i], temp_trial_fitness[i], X, y, linear_scaling)
-                    fitness_plus = temp_trial_fitness[i][0]
-                    temp_trial_structures[i, j] = original_value - epsilon
-                    evaluate_individual(temp_trial_structures[i], trial_constants[i], temp_trial_fitness[i], X, y, linear_scaling)
-                    fitness_minus = temp_trial_fitness[i][0]
-                    # Calculate the gradient using finite differences
-                    if not math.isinf(fitness_plus) and not math.isinf(fitness_minus):
-                        gradients[i, j] = ((fitness_plus - fitness_minus) / (epsilon))
-                    # else:
-                    #     print("inf")
-                    # Reset the structure to its original value
-                    temp_trial_structures[i, j] = original_value
+        # specify the methods for local search for both the constants and structures
+        structure_search = 'central'  # 'none' or 'forward' or 'central' or 'backward'
+        constants_search = 'central'  # 'none' or 'forward' or 'central' or 'backward'
 
-                elif finite_difference_method == 'central':
-                    # f'(x) = 1/(2h) * (f(x + h) - f(x - h)) : central difference method
-                    original_value = temp_trial_structures[i, j]
-                    temp_trial_structures[i, j] = original_value + epsilon
-                    evaluate_individual(temp_trial_structures[i], trial_constants[i], temp_trial_fitness[i], X, y, linear_scaling)
-                    fitness_plus = temp_trial_fitness[i][0]
-                    temp_trial_structures[i, j] = original_value - epsilon
-                    evaluate_individual(temp_trial_structures[i], trial_constants[i], temp_trial_fitness[i], X, y, linear_scaling)
-                    fitness_minus = temp_trial_fitness[i][0]
-                    # Calculate the gradient using finite differences
-                    if not math.isinf(fitness_plus) and not math.isinf(fitness_minus):
-                        gradients[i, j] = ((fitness_plus - fitness_minus) / (2 * epsilon))
-                    # else:
-                    #     print("inf")
-                    # Reset the structure to its original value
-                    temp_trial_structures[i, j] = original_value
-                else: # default to central difference method
-                    # f'(x) = 1/(2h) * (f(x + h) - f(x - h)) : central difference method
-                    original_value = temp_trial_structures[i, j]
-                    temp_trial_structures[i, j] = original_value + epsilon
-                    evaluate_individual(temp_trial_structures[i], trial_constants[i], temp_trial_fitness[i], X, y, linear_scaling)
-                    fitness_plus = temp_trial_fitness[i][0]
-                    temp_trial_structures[i, j] = original_value - epsilon
-                    evaluate_individual(temp_trial_structures[i], trial_constants[i], temp_trial_fitness[i], X, y, linear_scaling)
-                    fitness_minus = temp_trial_fitness[i][0]
-                    # Calculate the gradient using finite differences
-                    if not math.isinf(fitness_plus) and not math.isinf(fitness_minus):
-                        gradients[i, j] = ((fitness_plus - fitness_minus) / (2 * epsilon))
-                    # else:
-                    #     print("inf")
-                    # Reset the structure to its original value
-                    temp_trial_structures[i, j] = original_value
-            
+        # TODO: add structure and constant search to config, also make 2 different epsilons and learning rates
 
-        # Use the gradients to perform a local search on the structures
-        # if np.sum(gradients) != 0:
-        #     print("working")
+        structs_grad = None if structure_search == 'none' else finite_differencing(X, structure_search, temp_trial_fit,
+                                                                                   temp_trial_structs,
+                                                                                   temp_trial_consts, y,
+                                                                                   edit_structs=True)
 
-        for i in range(trial_structures.shape[0]):
-            for j in range(trial_structures.shape[1]):
-                trial_structures[i, j] = temp_trial_structures[i, j] + (gradients[i, j] * learning_rate)  # learning_rate is a hyperparameter to be defined
+        consts_grad = None if constants_search == 'none' else finite_differencing(X, constants_search, temp_trial_fit,
+                                                                                  temp_trial_consts,
+                                                                                  temp_trial_structs, y,
+                                                                                  edit_structs=False)
+        # update the parameters
+        update_parameters(trial_structures, structs_grad, temp_trial_structs, learning_rate)
+        update_parameters(trial_constants, consts_grad, temp_trial_consts, learning_rate)
 
+        # evaluate the population
         evaluate_population(trial_structures, trial_constants, trial_fitness, X, y, linear_scaling)
         return population_size
 
+    def update_parameters(param, grad, temp_param, lr):
+        for i in range(param.shape[0]):
+            for j in range(param.shape[1]):
+                param[i, j] = temp_param[i, j] + (grad[i, j] * lr) if grad is not None else temp_param[i, j]
+
+    def finite_differencing(X, finite_difference_method, fitness, to_edit, to_leave, y, edit_structs=False):
+        gradients = np.zeros_like(to_edit)
+        for i in range(to_edit.shape[0]):
+            for j in range(to_edit.shape[1]):
+                # Perturb the structure/constants positively and negatively
+                original_value = to_edit[i, j]
+
+                # f'(x) = 1/h * (f(x + h) - f(x)) : forward difference method
+                # f'(x) = 1/h * (f(x) - f(x - h)) : backward difference method
+                # f'(x) = 1/(2h) * (f(x + h) - f(x - h)) : central difference method
+
+                # compute first evaluation
+                if finite_difference_method == 'forward' or finite_difference_method == 'central':
+                    to_edit[i, j] = original_value + epsilon
+
+                if edit_structs:
+                    evaluate_individual(to_edit[i], to_leave[i], fitness[i], X, y, linear_scaling)
+                else:
+                    evaluate_individual(to_leave[i], to_edit[i], fitness[i], X, y, linear_scaling)
+
+                # Get MSE
+                fitness_first = fitness[i][0]
+
+                # reset to original value
+                to_edit[i, j] = original_value
+
+                # compute second evaluation
+                if finite_difference_method == 'backward' or finite_difference_method == 'central':
+                    to_edit[i, j] = original_value - epsilon
+
+                if edit_structs:
+                    evaluate_individual(to_edit[i], to_leave[i], fitness[i], X, y, linear_scaling)
+                else:
+                    evaluate_individual(to_leave[i], to_edit[i], fitness[i], X, y, linear_scaling)
+
+                # Get MSE
+                fitness_second = fitness[i][0]
+
+                # reset to original value
+                to_edit[i, j] = original_value
+
+                # Calculate the gradient using finite differences
+                multiplier = 2 if finite_difference_method == 'central' else 1
+                if not math.isinf(fitness_first) and not math.isinf(fitness_second):
+                    gradients[i, j] = ((fitness_first - fitness_second) / (multiplier * epsilon))
+        return gradients
+
     return perform_variation
-
-
-
